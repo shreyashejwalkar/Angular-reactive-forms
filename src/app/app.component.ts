4d1296bd917f94ed86636a5b45a2d0e1fe02907a
shreyashejwalkar <shreyashejwalkar1610@gmail.com>
2020-02-27 11:35:12 +0530

    Managing Control Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,32 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   title = 'reactive-forms';
+
+
   registrationForm = new FormGroup({
-    username : new FormControl('Shreya'),
-    password : new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
     confirmPassword: new FormControl(''),
-    address : new FormGroup({
-      city : new FormControl(),
-      state : new FormControl(),
-      postalCode : new FormControl()
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
     })
 
   });
+
+  loadApiData()
+  {
+    this.registrationForm.setValue({
+      username: 'Shreya',
+      password: 'test',
+      confirmPassword: 'test',
+      address: {
+        city: 'Nashik',
+        state: 'Maharashtra',
+        postalCode: '422101'
+      }
+    });
+
+  }
 }
