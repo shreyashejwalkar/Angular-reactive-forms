bca1a5a7c484ba7a406c48b71fa6b5534f538f88
shreyashejwalkar <shreyashejwalkar1610@gmail.com>
2020-02-28 14:48:22 +0530

    Conditional Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder, Validators } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name.validator';
@@ -9,23 +9,50 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  registrationForm : FormGroup;
+  ngOnInit(){
+    this.registrationForm = this.formbuilder.group({
+      username : ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      email : [''],
+      subscribe : [false],
+      password: [''],
+        confirmPassword: [''],
+        address: this.formbuilder.group({
+          city:[''],
+          state: [''],
+          postalCode: ['']
+        })
+     },{validator : PasswordValidator});
+
+     this.registrationForm.get('subscribe').valueChanges // get hold of subscribe element and access valuechange which return observable
+     .subscribe(checkedValue => //subscribe to observable
+      {  
+       const email = this.registrationForm.get('email');
+       //if checkbox is checked then set email to required else email is not required
+       if(checkedValue)
+       {
+         email.setValidators(Validators.required);
+       }
+       else
+       {
+         email.clearValidators();
+       }
+       // ensure correct status
+       email.updateValueAndValidity();
+     });
+  }
 
   constructor(private formbuilder: FormBuilder){}
   title = 'reactive-forms';
-  registrationForm = this.formbuilder.group({
-  username : ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
-  password: [''],
-    confirmPassword: [''],
-    address: this.formbuilder.group({
-      city:[''],
-      state: [''],
-      postalCode: ['']
-    })
- },{validator : PasswordValidator});
- 
+  
 get username(){
   return this.registrationForm.get('username');
+}
+
+get email(){
+  return this.registrationForm.get('email');
 }
   /* registrationForm = new FormGroup({
     username: new FormControl(''),
